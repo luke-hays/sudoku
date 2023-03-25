bdb81e21f3dc73f6bf488d5fb2a2b21da9335eb7
lhays <lhays9606@gmail.com>
2023-03-24 22:21:43 -0400

    add A* logic and tests

--- sudoku.py
+++ sudoku.py
@@ -1,6 +1,8 @@
 # Sudoku / n-Puzzle Solver - A* Algorithm
 # https://en.wikipedia.org/wiki/A*_search_algorithm
 
+from queue import PriorityQueue
+
 def formatGrid(grid):
   for row in range(len(grid)):
     for col in range(len(grid)):
@@ -10,10 +12,65 @@ def formatGrid(grid):
 def generator():
   return None
 
-def solver(grid):
-  formatGrid(grid)
+def gridInSolvedState(grid):
+  for row in range(len(grid)):
+    if None in grid[row]: return False
+    if (len(set(grid[row])) != len(grid[row])): return False
 
-  return None
+  for row in range(len(grid)):
+    colSet = set()
+    for col in range(len(grid)):
+      colSet.add(grid[col][row])
+    if len(colSet) != len(grid): return False
+  return True
+
+def getEmptyPos(grid):
+  for row in range(len(grid)):
+    for col in range(len(grid)):
+      if grid[row][col] == None:
+        return [row, col]
+      
+def getPriority(grid):
+  priority = 0
+  for row in range(len(grid)):
+    for col in range(len(grid)):
+      if grid[row][col] == None:
+        priority += 1
+  return priority
+
+def placementIsValid(n, pos, grid):
+  for row in range(len(grid)):
+    if grid[row][pos[1]] == n: return False
+  for col in range(len(grid)):
+    if grid[pos[0]][col] == n: return False
+  return True
+
+# With A* put our starting grid on top of a priority queue
+# Start from highest priority config and add to queue all configs that can be reached from that with move
+# We can try every number from 1 - grid length inclusive
+# We should only add states that are valid
+# Adding a number to the grid reduces the Nones - use as priority?
+def solver(grid):  
+  pq = PriorityQueue()
+
+  pq.put((getPriority(grid), grid))
+  
+  while not pq.empty():
+    state = pq.get()
+    priority = state[0]
+    newGrid = state[1]
+
+    if (gridInSolvedState(newGrid)): 
+      print(newGrid)
+      return newGrid
+
+    pos = getEmptyPos(newGrid)
+
+    for i in range(1, len(grid)+1):
+      if (placementIsValid(i, pos, newGrid)):
+        clone = [[y for y in x] for x in newGrid]
+        clone[pos[0]][pos[1]] = i
+        pq.put((priority, clone))
 
 def main():
   print('Hello World')
--- sudoku_test.py
+++ sudoku_test.py
@@ -2,6 +2,20 @@ import unittest # https://docs.python.org/3/library/unittest.html
 import sudoku
 
 class TestSudoku(unittest.TestCase):
+  def test_solved_checker(self):
+    emptyGrid = [[None for _ in range(4)] for _ in range(4)]
+    almostEmptyGrid = [[2,3,1,4],[4,1,3,2],[3,4,2,1],[1,2,4,None]]
+    duplicateRowGrid = [[2,3,1,5],[4,1,3,2],[3,4,2,1],[1,2,4,4]]
+    duplicateColumnGrid = [[2,3,1,4],[5,1,3,4],[3,4,2,1],[1,2,4,3]]
+    validSolvedGrid = [[2,3,1,4],[4,1,3,2],[3,4,2,1],[1,2,4,3]]
+
+    self.assertEqual(sudoku.gridInSolvedState(emptyGrid), False)
+    self.assertEqual(sudoku.gridInSolvedState(almostEmptyGrid), False)
+    self.assertEqual(sudoku.gridInSolvedState(duplicateRowGrid), False)
+    self.assertEqual(sudoku.gridInSolvedState(duplicateColumnGrid), False)
+    self.assertEqual(sudoku.gridInSolvedState(validSolvedGrid), True)
+
+
   @unittest.skip('')
   def test_generator_4x4(self):
     self.assertEqual(True, False)
@@ -16,7 +30,7 @@ class TestSudoku(unittest.TestCase):
 
   def test_solver_4x4(self):
     grid = [[None for _ in range(4)] for _ in range(4)]
-    solvedGrid = [[None for _ in range(4)] for _ in range(4)]
+    solvedGrid = [[2,3,1,4],[4,1,3,2],[3,4,2,1],[1,2,4,3]]
 
     grid[0][0] = 2
     grid[1][1] = 1
